8ed7b134ce722507dad0618241412e5b12fcafa3
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-02-25 15:23:20 +0000

    add leave and remove membership

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class MembershipController extends Controller
 {
+    public function remove(Membership $membership) {
+        $colocation = $membership->colocation;
+        if ($colocation->owner_id !== auth()->id()) {
+            abort(403);
+        }
 
+        $membership->update([
+            'left_at' => now()
+        ]);
+
+        return back();
+    }
+
+    public function leave() {
+
+        auth()->user()->activeMembership->update([
+            'left_at' => now()
+        ]);
+
+        return to_route('dashboard');
+    }
 }
