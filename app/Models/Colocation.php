25b850464028bb8e892ddc625c8de66939ecf935
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-02-26 11:23:30 +0000

    add expenses route

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -25,4 +25,12 @@ class Colocation extends Model {
         return $this->hasMany(Invitation::class);
     }
 
+    public function expenses() {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function categories() {
+        return $this->hasMany(Category::class);
+    }
+
 }
